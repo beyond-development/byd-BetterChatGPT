e1bc3f0e0c31a510c64ee0f3a672c4cb88137ba6
beyond_masuda <jun_masuda@beyondjapan.com>
2024-05-23 10:17:22 +0900

    feat: 利用モデルをturboに制限

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -19,15 +19,16 @@ Respond using Markdown.`;
 
 export const modelOptions: ModelOptions[] = [
   'gpt-3.5-turbo',
-  'gpt-3.5-turbo-16k',
-  'gpt-3.5-turbo-1106',
-  'gpt-3.5-turbo-0125',
-  'gpt-4',
-  'gpt-4-32k',
-  'gpt-4-1106-preview',
-  'gpt-4-0125-preview',
+  // 'gpt-3.5-turbo-16k',
+  // 'gpt-3.5-turbo-1106',
+  // 'gpt-3.5-turbo-0125',
+  // 'gpt-4',
+  // 'gpt-4-32k',
+  // 'gpt-4-1106-preview',
+  // 'gpt-4-0125-preview',
   'gpt-4-turbo',
-  'gpt-4-turbo-2024-04-09'
+  // 'gpt-4-turbo-2024-04-09',
+  // 'gpt-4o',
   // 'gpt-3.5-turbo-0301',
   // 'gpt-4-0314',
   // 'gpt-4-32k-0314',
@@ -53,6 +54,7 @@ export const modelMaxToken = {
   'gpt-4-0125-preview': 128000,
   'gpt-4-turbo': 128000,
   'gpt-4-turbo-2024-04-09': 128000,
+  'gpt-4o': 128000,
 };
 
 export const modelCost = {
@@ -123,6 +125,10 @@ export const modelCost = {
   'gpt-4-turbo-2024-04-09': {
     prompt: { price: 0.01, unit: 1000 },
     completion: { price: 0.03, unit: 1000 },
+  },
+  'gpt-4o': {
+    prompt: { price: 0.005, unit: 1000 },
+    completion: { price: 0.015, unit: 1000 },
   },
 };
 
