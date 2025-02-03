1938c6e36bf851af64c76292d2dade5ed9a2185b
beyondmasuda <jun_masuda@beyondjapan.com>
2025-02-03 18:27:53 +0900

    feat: rearrange model order

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -18,8 +18,10 @@ Carefully heed the user's instructions.
 Respond in Japanese using Markdown.`;
 
 export const modelOptions: ModelOptions[] = [
-  'o3-mini',
   'gpt-4o-mini',
+  'o3-mini',
+  'gpt-4o',
+  'gpt-4-turbo',
   'gpt-3.5-turbo',
   // 'gpt-3.5-turbo-16k',
   // 'gpt-3.5-turbo-1106',
@@ -28,9 +30,7 @@ export const modelOptions: ModelOptions[] = [
   // 'gpt-4-32k',
   // 'gpt-4-1106-preview',
   // 'gpt-4-0125-preview',
-  'gpt-4-turbo',
   // 'gpt-4-turbo-2024-04-09',
-  'gpt-4o',
   // 'gpt-4o-2024-05-13',
   // 'gpt-3.5-turbo-0301',
   // 'gpt-4-0314',
